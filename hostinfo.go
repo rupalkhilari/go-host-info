cf47ab890cf82a837b31d05bfbae77dd5c8bd599
Rupal Khilari <rupal.khilari@autodesk.com>
2017-11-17 15:35:39 -0800

    Testing all calls

--- hostinfo.go
+++ hostinfo.go
@@ -9,6 +9,13 @@ import (
 	"github.com/rupalkhilari/go-host-info/cloud/gcp"
 )
 
+type CloudProvider int
+const (
+	GCP CloudProvider = iota
+	AWS
+	UNKNOWN
+)
+
 func main() {
 	if cname, err := net.LookupCNAME("www.google.com"); err != nil {
 		fmt.Printf("lookup failed: %q\n", err)
@@ -16,6 +23,18 @@ func main() {
 		fmt.Printf("lookup success: %s\n", cname)
 	}
 	fmt.Println(IsCloudInstance())
+	if IsCloudInstance() == true {
+		provider := DetermineCurrentCloud()
+		switch provider {
+		case GCP:
+			RunGCPCloudFuncs()
+		case AWS:
+			RunAWSCloudFuncs()
+		default:
+			fmt.Println("No implementation available")
+		}
+	}
+
 }
 
 // Determines if the host is a cloud instance.
@@ -27,7 +46,37 @@ func IsCloudInstance() bool {
 	} else if gcp.HasMetadataHost() == true {
 		fmt.Println("This is a gcp host")
 		return true
+	} else {
+		fmt.Println("This host is not on any known cloud provider")
 	}
-	fmt.Println("This host is not on any known cloud provider")
 	return false
+}
+
+func DetermineCurrentCloud() CloudProvider {
+	if aws.HasMetadataHost() == true {
+		return AWS
+	} else if gcp.HasMetadataHost() == true {
+		return GCP
+	}
+	return UNKNOWN
+}
+// Runs all the cloud host info to retrieve details.
+
+func RunGCPCloudFuncs() {
+
+	gcp.FQDN()
+	gcp.PublicHostname()
+	gcp.Hostname()
+	gcp.LocalIPAddress()
+	gcp.PublicIPAddress()
+	gcp.Id()
+	gcp.Zone()
+	gcp.Type()
+
+	gcp.IsPreemptible()
+	gcp.Tags()
+}
+
+func RunAWSCloudFuncs() {
+
 }
